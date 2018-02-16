6a020ca333889fcc8275beec68484c791eaa3a61
lillejulaften <fredrik.holst@elmatica.com>
2018-02-16 09:42:22 +0100

    Bugfix: Fixed serializer

--- app/serializers/manufacturer_serializer.rb
+++ app/serializers/manufacturer_serializer.rb
@@ -1,11 +1,9 @@
 class ManufacturerSerializer < ActiveModel::Serializer
   attributes :name, :verified, :description, :location, :materials
   #has_many :materials, include: :name
-end
   def materials
     object.materials.map do |material|
       ManufactureMaterialSerializer.new(material, scope: scope, root: true, manufacturer: object)
     end
   end
-
 end
