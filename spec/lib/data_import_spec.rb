886ea767ec7922df21f20d0d8d041e3249c6633d
Max Brosnahan <maximilianbrosnahan@gmail.com>
2019-04-03 10:02:53 +0200

    Update import spec

--- spec/lib/data_import_spec.rb
+++ spec/lib/data_import_spec.rb
@@ -1,105 +1,25 @@
 require "rails_helper"
 
 RSpec.describe "Importing csv into db" do
-  subject { described_class.new }
-  let(:path) { Rails.root.join("lib", "data", "materials.csv") }
-  let(:data) { File.open(path).read }
-  let(:manufacturer_names) {
-    [
-      "AISMALIBAR",
-      "Allstar Zs",
-      "AMC (TW)",
-      "Isola",
-      "Arlon",
-      "Cambridge Nanotherm",
-      "CeramTec",
-      "CHAOSHUN",
-      "Chin-Shi Electronic Materials TW",
-      "Dongli (HeShan DongLi Electronic)",
-      "Doosan",
-      "Dongguan Langban",
-      "Dupont",
-      "Electra",
-      "Elektro Isola",
-      "EMC",
-      "Eternal",
-      "Fotochem",
-      "Gin Hwa",
-      "Goo Chemicals",
-      "GORE",
-      "Grace",
-      "Grand-Tag",
-      "Greencure",
-      "Greenmark",
-      "HANWHA",
-      "HongDa",
-      "Hitachi",
-      "Huntsman",
-      "Innox",
-      "ITEQ",
-      "Jiangsu Lianxin",
-      "Jumbo",
-      "Kuangshun",
-      "Kinwong",
-      "Mitsubishi",
-      "Nam Hing",
-      "Nanya",
-      "Onstatic",
-      "Peters",
-      "PIC",
-      "PTTC",
-      "Rogers",
-      "ROHM and HAAS",
-      "Rongda",
-      "Rootlike",
-      "Shengyi",
-      "Shinemore",
-      "Sun Star",
-      "SunChemical",
-      "Taconic",
-      "Taiflex",
-      "Taiyo",
-      "Tamura",
-      "Thinflex",
-      "TOYO CHEM",
-      "Zhejiang Huazheng",
-      "Ventec",
-      "KREMPEL GmbH",
-      "Kingboard",
-      "Bergquist",
-      "TUC",
-      "Laird",
-      "Lamitec-Dielektra GmbH",
-      "MSC Polymer",
-      "Nanmei",
-      "Nelco",
-      "Nippon Steel/Espanex",
-      "Panasonic",
-      "Polytronics",
-      "Shanghai global (Goldenmax International)",
-      "Shengyi",
-      "Sumitomo",
-      "TaiFlex",
-      "Totking",
-      "Uniplus",
-      "Unires Chemicals TW",
-      "Yeyo",
-      "WL (China)",
-    ]
-  }
-
-  before do
-    manufacturer_names.each do |name|
-      create(:manufacturer, name: name)
-    end
-  end
+  let(:manufacturer_path) { Rails.root.join("lib", "data", "manufacturers.csv") }
+  let(:manufacturer_data) { File.open(manufacturer_path).read }
+  let(:material_path) { Rails.root.join("lib", "data", "materials.csv") }
+  let(:material_data) { File.open(material_path).read }
 
   it "imports successfully" do
-    CsvToDb.new(data).load_into_db
+    ManufacturerCsvToDb.new(manufacturer_data).load_into_db
+    MaterialCsvToDb.new(material_data).load_into_db
   end
 
-  it "dumps into the same content" do
-    CsvToDb.new(data).load_into_db
-    expect(DbToCsv.new.to_csv).to eql(data)
+  context "data is loaded into DB" do
+    before do
+      ManufacturerCsvToDb.new(manufacturer_data).load_into_db
+      MaterialCsvToDb.new(material_data).load_into_db
+    end
+
+    it "dumps into the same content" do
+      expect(ManufacturerDbToCsv.new.to_csv).to eql(manufacturer_data)
+      expect(MaterialDbToCsv.new.to_csv).to eql(material_data)
+    end
   end
 end
