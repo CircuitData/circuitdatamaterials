22879cd5da8751ab611b560e0fef1bb7f3cf9982
Max Brosnahan <maximilianbrosnahan@gmail.com>
2019-04-12 07:41:29 +0200

    Ensure that materials are listed for a manufacturer

--- spec/features/manufacturer_spec.rb
+++ spec/features/manufacturer_spec.rb
@@ -2,11 +2,20 @@ require "rails_helper"
 
 RSpec.describe "Manufacturers" do
   let!(:manufacturer) { create(:manufacturer, name: "big ost") }
+  let!(:material) { create(:material, manufacturer: manufacturer, name: "gouda") }
 
   it "displays a list of all the manufacturers" do
     visit "/"
     click_on "Manufacturers"
 
     expect(page).to have_content("big ost")
+  end
+
+  it "links to the list of materials associated to the manufacturer" do
+    visit "/"
+    click_on "Manufacturers"
+    click_on "big ost"
+
+    expect(page).to have_content("gouda")
   end
 end
