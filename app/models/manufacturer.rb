c02c13e48f23ee2c8998e35d55d4469ff25a45bf
Elias Bakken <elias@iagent.no>
2019-05-27 13:14:30 +0200

    Added search field for manufacturer

--- app/models/manufacturer.rb
+++ app/models/manufacturer.rb
@@ -2,4 +2,8 @@ class Manufacturer < ApplicationRecord
   has_many :materials
 
   validates :name, presence: true
+
+  def self.names
+    all.map{|material| [material.name, material.id]}
+  end
 end
