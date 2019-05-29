d72649f33723c2a2b5dd904869b6fd9a70d43511
Elias Bakken <elias@iagent.no>
2019-05-29 15:06:45 +0200

    Added table for different attributes

--- app/controllers/materials_controller.rb
+++ app/controllers/materials_controller.rb
@@ -1,6 +1,19 @@
 
 class MaterialsController < ApplicationController
   include Pagy::Backend
+
+  MATERIAL_ATTRIBUTES = {
+    "default" => ["id", "function", "group", "link", "ipc_standard"],
+    "conductive" => ["foil_roughness", "flexible", "thermal_conductivity"],
+    "dielectric" => ["flexible", "ul94", "cti", "df", "dielectric_breakdown",
+      "dk", "electric_strength", "mot", "t260", "t280", "t300", "td_min",
+      "tg_min", "thermal_conductivity", "water_absorption", "z_cte",
+      "z_cte_after_tg", "z_cte_before_tg", "ipc_slash_sheet", "finish"],
+    "soldermask" => ["flexible", "thermal_conductivity", "ipc_sm_840_class"],
+    "stiffener" => [],
+    "final_finish" => []
+  }
+
   def index
     query = MaterialSearch.new(search_params.to_h)
 
@@ -11,9 +24,8 @@ class MaterialsController < ApplicationController
 
   def show
     @material = Material.find(params[:id])
-    @attributes = @material.attributes.except(
-      "name", "manufacturer_id", "created_at", "updated_at"
-    )
+    @attributes = @material.attributes.slice(
+      *MATERIAL_ATTRIBUTES["default"]+MATERIAL_ATTRIBUTES[@material.attributes["function"]])
   end
 
   def datasheet
