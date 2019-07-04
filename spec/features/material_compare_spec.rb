7a1cb5f30b97e83d3b23ad4a64c31bbe0977cb00
Elias Bakken <elias@iagent.no>
2019-07-04 12:29:23 +0200

    Fixed faulty assignment

--- spec/features/material_compare_spec.rb
+++ spec/features/material_compare_spec.rb
@@ -15,12 +15,8 @@ RSpec.describe "Comparing two materials" do
   }
 
   scenario "Name of both materials is displayed" do
+    session[:compare] = [material1.to_param, material2.to_param]
     visit "/materials/compare"
-
-    page.all(:fillable_field, 'material_ids[]')[0].set(material1.to_param)
-    page.all(:fillable_field, 'material_ids[]')[1].set(material2.to_param)
-    click_button("Compare")
-
     expect(page).to have_content("Cheese")
     expect(page).to have_content("Bacon")
   end
