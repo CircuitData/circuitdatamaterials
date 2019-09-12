beefa97b64b0663c39bef123b280a913617643dd
Max Brosnahan <maximilianbrosnahan@gmail.com>
2019-09-12 08:22:00 +0200

    Remove datasheets that are no longer valid

--- spec/lib/datasheet_spec.rb
+++ spec/lib/datasheet_spec.rb
@@ -2,8 +2,8 @@ require "rails_helper"
 
 RSpec.describe Datasheet do
   let(:material) { double(:material, manufacturer_name: manufacturer, name: name) }
-  let(:manufacturer) { "Arlon" }
-  let(:name) { "25Fr" }
+  let(:manufacturer) { "Kingboard" }
+  let(:name) { "HF-140" }
 
   subject { described_class.new(material) }
 
@@ -17,14 +17,14 @@ RSpec.describe Datasheet do
   end
 
   describe "#file_path" do
-    let(:expected_path) { Rails.root.join("lib", "datasheets", "arlon", "25fr.pdf") }
+    let(:expected_path) { Rails.root.join("lib", "datasheets", "kingboard", "hf_140.pdf") }
     it "is the correct file path" do
       expect(subject.file_path).to eql(expected_path)
     end
 
     context "when name has spaces" do
       let(:name) { "piz za" }
-      let(:expected_path) { Rails.root.join("lib", "datasheets", "arlon", "piz_za.pdf") }
+      let(:expected_path) { Rails.root.join("lib", "datasheets", "kingboard", "piz_za.pdf") }
 
       it "replaces spaces with underscores" do
         expect(subject.file_path).to eql(expected_path)
@@ -33,7 +33,7 @@ RSpec.describe Datasheet do
 
     context "when manufacturer has spaces" do
       let(:manufacturer) { "Bar ry" }
-      let(:expected_path) { Rails.root.join("lib", "datasheets", "bar_ry", "25fr.pdf") }
+      let(:expected_path) { Rails.root.join("lib", "datasheets", "bar_ry", "hf_140.pdf") }
 
       it "replaces spaces with underscores" do
         expect(subject.file_path).to eql(expected_path)
@@ -42,7 +42,7 @@ RSpec.describe Datasheet do
 
     context "when manufacturer has non alpha numeric characters" do
       let(:manufacturer) { "Bar&-ry" }
-      let(:expected_path) { Rails.root.join("lib", "datasheets", "bar_ry", "25fr.pdf") }
+      let(:expected_path) { Rails.root.join("lib", "datasheets", "bar_ry", "hf_140.pdf") }
 
       it "replaces characters with a single underscore" do
         expect(subject.file_path).to eql(expected_path)
